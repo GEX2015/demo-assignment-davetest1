c067400a1fbebf9a949a72de45bad4f648f61855
Dave Burchill <dave@burchill.ca>
2016-05-08 15:06:58 -0700

    initial commit

---
+++ jacks or better/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jacks or better
+//
+//  Created by Dave Burchill on 2016-05-08.
+//  Copyright © 2016 Dave Burchill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
